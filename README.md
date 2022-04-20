5d7c8184cd81ecc74d765ad43727de52c46fcf4e
Oscar Enrique Estrada Garcia <oenriqueg@gmail.com>
2022-04-20 18:45:08 -0500

    To production version

---
+++ README.md
@@ -0,0 +1,3 @@
+# extract_data_from_xml
+
+This script will extract automated information from several xml files (Invoices for Mexico in this case), we need to obtain all employees and their amounts and save in a excel file
---
+++ extract_data_from_xml_invoice.py
@@ -0,0 +1,38 @@
+import os
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+cwd = os.getcwd()
+files = os.listdir(cwd+'/xmls/')
+
+for file in files:
+    # Abrimos y parseamos el fichero XML
+    #with open(file, 'rb') as f:
+    tree = ET.parse('xmls/'+file)
+    
+    name = file.split('.')
+    filename = name[0]
+    print(f'Archivo procesado:  {filename}.xml')
+    
+    # Obtenemos el elemento raíz del fichero XML,
+    # es decir, la etiqueta <Data>
+    factura = tree.getroot()
+
+    # Creamos el array vacío que contendrá los
+    # datos agrupados de los dependientes
+    lista_de_empleados = []
+
+    # Iteramos sobre cada dependiente del fichero
+    for empleado in factura[4][0]:
+        # Almacenamos en una lista todos los
+        # atributos de un dependiente
+        for atributo in empleado:
+            lista_atributos = [atributo.attrib.values() for atributo in empleado]
+
+
+    dataframe = pd.DataFrame(lista_atributos, columns=['CURP', 'FECHA', 'ID', 'IMPORTE','NOMBRE','IMSS','RFC'])
+    dataframe['IMPORTE'] = dataframe['IMPORTE'].apply(pd.to_numeric, errors='coerce').fillna(0)
+    dataframe.set_index('NOMBRE', inplace = True)
+    writer = pd.ExcelWriter(f'resultado_{filename}'+'.xlsx', engine='xlsxwriter', datetime_format='dd/Mmm/yyyy')
+    dataframe.to_excel(writer,'empleados', index=True)
+    writer.save()
